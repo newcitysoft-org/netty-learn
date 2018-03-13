66ec2fe7ce6df4c6c3c6e6e07782f2468e9ef330
tianlixin <newcitysoft@163.com>
2018-03-13 22:23:47 +0800

    使用udp实现群聊功能

---
+++ src/main/java/com/newcitysoft/study/net/udp/ChatReceive.java
@@ -0,0 +1,29 @@
+package com.newcitysoft.study.net.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class ChatReceive extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            // 建立udp的服务，要监听一个端口
+            DatagramSocket socket = new DatagramSocket(6789);
+            // 准备空的数据包存储数据
+            byte[] buf = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            boolean flag = true;
+            while(flag) {
+                socket.receive(packet);
+                System.out.println(packet.getAddress()+"："+new String(buf, 0, packet.getLength()));
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
