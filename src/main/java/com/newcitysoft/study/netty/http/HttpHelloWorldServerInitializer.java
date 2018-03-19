66e87d412f7a82dde884cbfb2270e6731257f9bf
tianlixin <newcitysoft@163.com>
2018-03-19 15:49:19 +0800

    基于netty实现Http协议。

---
+++ src/main/java/com/newcitysoft/study/netty/http/HttpHelloWorldServerInitializer.java
@@ -0,0 +1,16 @@
+package com.newcitysoft.study.netty.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    public void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpHelloWorldServerHandler());
+    }
+}
