9b4fec31bd8961d711bf552a1adff5700593ccbe
tianlixin <newcitysoft@163.com>
2018-03-20 14:10:56 +0800

    增加官方开放的netty实现的websocket协议，并根据版本号修改代码，并测试客户端。

---
+++ src/main/java/com/newcitysoft/study/netty/websocketx/server/WebSocketServer.java
@@ -0,0 +1,38 @@
+package com.newcitysoft.study.netty.websocketx.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public final class WebSocketServer {
+
+//    static final boolean SSL = System.getProperty("ssl") != null;
+    static final boolean SSL = false;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8443" : "8080"));
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .handler(new LoggingHandler(LogLevel.INFO))
+             .childHandler(new WebSocketServerInitializer(SSL));
+
+            Channel ch = b.bind(PORT).sync().channel();
+
+            System.out.println("Open your web browser and navigate to " +
+                    (SSL? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
