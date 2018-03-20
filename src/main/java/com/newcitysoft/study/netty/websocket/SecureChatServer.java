16b23a1c8461df5ab83367b61dacedef199ce8fb
tianlixin <newcitysoft@163.com>
2018-03-20 12:50:28 +0800

    增加WebSocket下的进行加密的方式穿传输信息。

---
+++ src/main/java/com/newcitysoft/study/netty/websocket/SecureChatServer.java
@@ -0,0 +1,45 @@
+package com.newcitysoft.study.netty.websocket;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Listing 12.7 Adding encryption to the ChatServer
+ *
+ * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ */
+public class SecureChatServer extends ChatServer {
+    private final SslContext context;
+
+    public SecureChatServer(SslContext context) {
+        this.context = context;
+    }
+
+    @Override
+    protected ChannelInitializer<Channel> createInitializer(
+        ChannelGroup group) {
+        return new SecureChatServerInitializer(group, context);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = Integer.parseInt("7777");
+        SelfSignedCertificate cert = new SelfSignedCertificate();
+        SslContext context = SslContext.newServerContext(
+                cert.certificate(), cert.privateKey());
+        final SecureChatServer endpoint = new SecureChatServer(context);
+        ChannelFuture future = endpoint.start(new InetSocketAddress(port));
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                endpoint.destroy();
+            }
+        });
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+}
