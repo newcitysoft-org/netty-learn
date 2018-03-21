597489d7d4f87aa72f2900d9fb8434376601badd
tianlixin <newcitysoft@163.com>
2018-03-21 16:05:43 +0800

    修改接收文件代码。

--- src/main/java/com/newcitysoft/study/netty/filetransfer/client/FileClientHandler.java
+++ src/main/java/com/newcitysoft/study/netty/filetransfer/client/FileClientHandler.java
@@ -4,10 +4,16 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author lixin.tian@renren-inc.com
@@ -17,35 +23,59 @@ public class FileClientHandler extends ChannelHandlerAdapter {
 
     private static String path = "D:\\data\\socket\\client\\123.txt";
 
+    private boolean first = true;
+    private FileOutputStream fos;
+    private BufferedOutputStream bufferedOutputStream;
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(path);
+        first = true;
+        System.out.println(ctx.channel().localAddress().toString() + " channelActive");
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         System.out.println(ctx.channel().localAddress().toString() + " channelInactive");
+        // 关闭流
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
+        first = false;
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        RandomAccessFile file = new RandomAccessFile("d:\\test\\456.txt", "rw");
-        FileChannel channel = file.getChannel();
-        ByteBuffer byteBuffer = ByteBuffer.allocate(128);
-
-        ByteBuf buf = (ByteBuf) msg;
-        byteBuffer.put(buf.getByte(buf.readableBytes()));
-        byteBuffer.flip();
-        while(byteBuffer.hasRemaining()) {
-            channel.write(byteBuffer);
+        // 第一次接收信息只创建文件
+        if (first) {
+            System.out.println("创建文件");
+            first = false;
+            File file = new File("D://test//" + new SimpleDateFormat("yyyymmddhhmmss").format(new Date()) + ".txt");
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                fos = new FileOutputStream(file);
+                bufferedOutputStream = new BufferedOutputStream(fos);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            ctx.writeAndFlush(path);
         }
-        System.out.println(channel.position());
-        close(file, channel);
-    }
 
-    public static void close(RandomAccessFile file, FileChannel channel) throws IOException {
-        channel.close();
-        file.close();
+        // 开始处理文件信息
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] bytes = new byte[buf.readableBytes()];
+        buf.readBytes(bytes);
+        System.out.println("本次接收内容长度：" + msg.toString().length());
+        try {
+            bufferedOutputStream.write(bytes, 0, bytes.length);
+            buf.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
